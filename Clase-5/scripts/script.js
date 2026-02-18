e5551bc044aea86b79ed3678b5a4ab80ad2a5eff
Shiros <99361326+Shiroses@users.noreply.github.com>
2026-02-18 00:09:57 -0500

    Feat :construction: Made progress and upgraded the project

---
+++ Clase-5/scripts/script.js
@@ -0,0 +1,45 @@
+function mensajeAsync(texto, tiempo) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(texto);
+    }, tiempo);
+  });
+}
+console.log("Iniciando...");
+
+mensajeAsync("Hola, este es tu mensaje asíncrono", 2000)
+  .then((mensaje) => {
+    console.log(mensaje);
+  })
+  .finally(() => {
+    console.log("Proceso finalizado");
+  });
+
+
+  const verificarNumeroAsync = (numero) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (numero % 2 === 0) {
+        resolve("Número válido");
+      } else {
+        reject("Número inválido");
+      }
+    }, 500);
+  });
+};
+
+verificarNumeroAsync(4)
+  .then((mensaje) => {
+    console.log("Éxito (4):", mensaje);
+  })
+  .catch((error) => {
+    console.error("Error (4):", error);
+  });
+
+  verificarNumeroAsync(5)
+  .then((mensaje) => {
+    console.log("Éxito (5):", mensaje);
+  })
+  .catch((error) => {
+    console.error("Error (5):", error);
+  });
--- proyecto-1/scripts/script.js
+++ proyecto-1/scripts/script.js
@@ -1,130 +1,61 @@
+let admin = 1234;
 let input;
-const roles = ["camper", "trainer", "coordinador"]
-const campers = []
 
-function coordinador_page () {
+//asi que esta funcion es la que nos permite iniciar sesion y tiene una forma estandar(?) de manejar inputs incorrectos
+function login() {
     while (true) {
-        input = prompt("Que operacion necesita realizar \n1. Añadir camper\n2. Editar camper\n3. Rutas\n0. Cancelar").toString().toLowerCase()
-        switch (input){
-            case "1":
-                coordinador_register_page()
-                continue
-            case "2":
-                coordinador_edit_page()
-                continue
-            case "3":
-                routes()
-            case "0":
-                input = prompt("Desea salir? (Y/N)").toLowerCase()
+        input = Number(prompt("Escoja su rol\n  1. Camper\n  2. Trainer\n  3. Coordinador\n  0. Salir"));
+
+        switch (input) {
+            case 1:
+                alert("entro a camper");
+                continue;
+            case 2:
+                alert("entro a trainer");
+                continue;
+            case 3:
+                alert("entro a coordinador");
+                camper.registrar()
+                continue;
+            case 0:
+                input = prompt("Desea salir? Y/N").toLowerCase();
                 if (input == "y") {
-                    return
-                }else {
-                    continue
-                }
+                    return alert("Ha salido satisfactoriamente");
+                };
             default:
-                alert("Opcion invalida, Intente nuevamente")
+                alert("Opcion invalida, intenta nuevamente");
+                continue
         }
     }
-}
+};
 
-function routes() {
-    input = prompt("Bienvenido a rutas, deseas:\n1. Añadir a un camper\n2. Añadir una nueva ruta")
-
-    switch (input) {
-        case "1":
-            input = prompt("A que camper le desea añadir?")
-        
-        case "2": 
-            input = prompt("Ingrese el nombre de la ruta")
-
-        case "3":
-            alert("Las rutas son:")
-        
-        default:
-            alert("Opcion no reconocida, intente nuevamente")
-    }
-}
-
-function coordinador_edit_page() {
-    let nCamper
-    let found = false
-    input = prompt("ingrese el nombre del camper a editar").toString().toLowerCase()
-    if (input === null || input === "") {
-        alert("Accion cancelada")
-        return
-    }
-    for (let camperN = 0; camperN < campers.length; camperN++){
-        if (input == campers[camperN][0]){
-            nCamper = camperN
-            found = true
+//Aqui declaramos a nuestra clase contructora de camper es la fabrica donde podemos agarrar moldes y crear
+class camper {
+    //esta es nuestra "base de datos" es temporal y se borra cuando se actualiza la pagina
+    static listaCampers = [];
+    //nuestro molde es lo que le da la forma y es el estandar de un camper
+    constructor(info) {
+        Object.assign(this, info);
+        this.notas = {
+            python : 0,
+            java : 0,
+            github : 0
         }
     }
-    if (found == false){
-        alert("Usuario no encontrado")
-    }else {
-        input = prompt("Que desea editar?\n1. nombre\n2. apellido\n3. No. identidad\n4. Dirección\n5. Acudiente\n6. Contacto\n7. Estado\n8. Riesgo") - 1
-        campers[nCamper][input] = prompt("Ingrese el nuevo campo")
-        console.log(campers[nCamper])
-    }
-}
+    //con esta funcion estamos replicando una fabrica donde le mandamos instrucciones y luego guardamos(temporalmente) 
+    static registrar() {
+        const campos = ["nombre", "apellido"];
+        let datosRecogidos = {};
 
-function coordinador_register_page(){
-    let tempCamper = []
-    try {
-        let newCamper = [
-            names = "nombre",
-            surName = "apellido",
-            id = "cedula",
-            address = "dirección de residencia",
-            attendant = "acudiente",
-            contact = "numero de contacto",
-            status = "estado",
-            risk = "riesgo"
-        ]
-        for (let i = 0; i < newCamper.length; i++){
-            infoInput = prompt("Ingrese su " + newCamper[i]).toLowerCase();
-            while (infoInput === null || infoInput === "") {
-                infoInput = prompt("El espacio no puede estar vacio\nIntente nuevamente")
-            }
-            tempCamper.push(infoInput)
+        for (let campo of campos) {
+            let infoInput = prompt(`Ingrese su ${campo}`)
+            datosRecogidos[campo] = infoInput.toLowerCase()
         }
-        console.log(tempCamper)
-        campers.push(tempCamper)
-
-    }catch{
-        alert("El usuario ha cancelado la operación")
-        tempCamper = []
-        return
+        const nuevo = new camper(datosRecogidos);
+        this.listaCampers.push(nuevo);
+        alert("Camper reclutado correctamente!")
+        console.table(this.listaCampers);
     }
-}
-
-while (true) {
-    try {
-        input = prompt("Ingresa tu rol! (camper, trainer, coordinador)").toLowerCase();
-        console.log(input)
-        if (!roles.includes(input)) {
-        alert("intenta nuevamente! code 1")
-            continue
-        }
-    }catch{
-        alert("intenta nuevamente! code 2")
-    }
-    break
-}
-
-switch(input){
-    case "camper":
-        alert("Camper")
-        break
-
-    case "trainer":
-        alert("trainer")
-        break
-
-    case "coordinador":
-        coordinador_page()
-        break
-
-    default:
-        alert("Input incorrecto, Intente nuevamente")
-}
+};
+//aqui iniciamos el programa
+login()
